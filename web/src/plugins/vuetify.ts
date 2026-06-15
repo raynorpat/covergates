5af2086ed4171a732edc0206ecebf0987c2a3f7c
Pat Raynor <raynorpat@raynorpat.com>
2026-06-14 22:14:49 -0400

    web: vuetify 3 theme

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -1,4 +1,20 @@
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
-export default createVuetify()
+export default createVuetify({
+  icons: { defaultSet: 'mdi', aliases, sets: { mdi } },
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#00838F', // cyan darken-3
+          secondary: '#212121', // grey darken-4
+          accent: '#546E7A', // blue-grey darken-1
+          error: '#B71C1C' // red darken-4
+        }
+      }
+    }
+  }
+})

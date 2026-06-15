13a5168b1e59bc1b05302c31bc5ed615ed225ff7
Pat Raynor <raynorpat@raynorpat.com>
2026-06-14 22:27:11 -0400

    web: remove vue 2 leftovers and update frontend ci

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,5 +1,9 @@
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-recommended', '@vue/eslint-config-typescript'],
-  parserOptions: { ecmaVersion: 'latest' }
+  extends: ['plugin:vue/vue3-essential', '@vue/eslint-config-typescript'],
+  parserOptions: { ecmaVersion: 'latest' },
+  rules: {
+    // Route views and single-purpose components are intentionally single-word.
+    'vue/multi-word-component-names': 'off'
+  }
 }
